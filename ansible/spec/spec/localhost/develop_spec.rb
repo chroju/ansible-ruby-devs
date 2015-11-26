fd5ad95f0fd426ce19bc2d84ca49b9ef742368bc
chroju <chor.chroju@gmail.com>
2015-11-26 22:02:00 +0900

    [tweak] sshd_config spec regexp changed

--- ansible/spec/spec/localhost/develop_spec.rb
+++ ansible/spec/spec/localhost/develop_spec.rb
@@ -17,12 +17,12 @@ describe file("/home/#{property[:username]}/.ssh/authorized_keys") do
 end
 
 describe file("/etc/ssh/sshd_config") do
-  its(:content) { should match /Protocol 2/ }
-  its(:content) { should match /PermitRootLogin no/ }
-  its(:content) { should match /PasswordAuthentication no/ }
-  its(:content) { should match /PubkeyAuthentication yes/ }
-  its(:content) { should match /MaxAuthTries 3/ }
-  its(:content) { should match /MaxStartups 3:90:5/ }
+  its(:content) { should match /^Protocol 2$/ }
+  its(:content) { should match /^PermitRootLogin no$/ }
+  its(:content) { should match /^PasswordAuthentication no$/ }
+  its(:content) { should match /^PubkeyAuthentication yes$/ }
+  its(:content) { should match /^MaxAuthTries 3$/ }
+  its(:content) { should match /^MaxStartups 3:90:5$/ }
 end
 
 describe selinux do
@@ -40,7 +40,7 @@ describe user('develop') do
 end
 
 [".zshrc", ".vimrc", ".zshenv"].each do |key|
-  describe file("#{key}") do
+  describe file("/home/develop/#{key}") do
     it { should exist }
   end
 end
